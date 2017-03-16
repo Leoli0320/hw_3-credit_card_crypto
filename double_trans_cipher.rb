94709a27635b71e40c1a35e1c45f321833ae5922
LeoLee <leo.li@iss.nthu.edu.tw>
2017-03-16 22:50:15 +0800

    Complete!!!!

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,4 +1,6 @@
 require 'Matrix'
+
+# This library can encrypt and decrypt using DoubleTransposition method
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
@@ -9,38 +11,39 @@ module DoubleTranspositionCipher
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
     stringify = document.to_s
-    a = Math.sqrt(stringify.length)
-    num_row = stringify.length <= a.ceil * (a.ceil - 1) ? a.ceil - 1 : a.ceil
-    num_col = a.ceil
-    origin_m = Matrix.build(num_row, num_col).with_index { |_, i| stringify[i] }
+    r_c = compute_number_of_rows_cols(stringify)
+    origin_m = Matrix.build(r_c[0], r_c[1]).with_index { |_, i| stringify[i] }
     # Generating Keys
-    row_key = (1..num_row).to_a.shuffle(random: Random.new(key))
-    col_key = (1..num_col).to_a.shuffle(random: Random.new(key + 1))
+    row_key = (1..r_c[0]).to_a.shuffle(random: Random.new(key))
+    col_key = (1..r_c[1]).to_a.shuffle(random: Random.new(key + 1))
     # Encrypt with keys
-    enc_m = Matrix.build(num_row, num_col) do |row, col|
-      origin_m.row(row_key[row] - 1)[col_key[col] - 1]
-    end
-    enc_m.to_a.join
+    Matrix.build(r_c[0], r_c[1]) do |row, col|
+      value = origin_m.row(row_key[row] - 1)[col_key[col] - 1]
+      value.nil? ? ' ' : value
+    end.to_a.join
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
-    a = Math.sqrt(ciphertext.length)
-    numof_r = ciphertext.length <= a.ceil * (a.ceil - 1) ? a.ceil - 1 : a.ceil
-    numof_c = a.ceil
-    enc_m = Matrix.build(numof_r, numof_c).with_index { |_, i| ciphertext[i] }
+    r_c = compute_number_of_rows_cols(ciphertext)
+    enc_m = Matrix.build(r_c[0], r_c[1]).with_index { |_, i| ciphertext[i] }
     # Generating Keys
-    key_row = (1..numof_r).to_a.shuffle(random: Random.new(key))
-    key_col = (1..numof_c).to_a.shuffle(random: Random.new(key + 1))
+    key_row = (1..r_c[0]).to_a.shuffle(random: Random.new(key))
+    key_col = (1..r_c[1]).to_a.shuffle(random: Random.new(key + 1))
     # Generate Decrypt Keys
-    d_key_row = []
-    d_key_col = []
+    d_key_row = [], d_key_col = []
     key_row.each_with_index { |value, i| d_key_row[value - 1] = i + 1 }
     key_col.each_with_index { |value, i| d_key_col[value - 1] = i + 1 }
     # Decrypt with keys
-    dec_m = Matrix.build(numof_r, numof_c) do |row, col|
+    Matrix.build(r_c[0], r_c[1]) do |row, col|
       enc_m.row(d_key_row[row] - 1)[d_key_col[col] - 1]
-    end
-    dec_m.to_a.join
+    end.to_a.join.strip
+  end
+
+  def self.compute_number_of_rows_cols(stringify)
+    a = Math.sqrt(stringify.length)
+    num_row = stringify.length <= a.ceil * (a.ceil - 1) ? a.ceil - 1 : a.ceil
+    num_col = a.ceil
+    [num_row, num_col]
   end
 end
