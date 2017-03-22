6466629b6f5d0911ac73de9f108654835afeb45c
LeoLee <leo.li@iss.nthu.edu.tw>
2017-03-22 17:03:23 +0800

    ADD Martins code

--- credit_card.rb
+++ credit_card.rb
@@ -1,14 +1,14 @@
 require_relative './luhn_validator.rb'
 require 'json'
+require 'rbnacl/libsodium'
 
 class CreditCard
-  # TODO: mixin the LuhnValidator using an 'include' statement
   include LuhnValidator
+
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
     @number = number
     @expiration_date = expiration_date
     @owner = owner
@@ -17,10 +17,7 @@ class CreditCard
 
   # returns json string
   def to_json
-    {
-      number: @number, expiration_date: @expiration_date, owner: @owner,
-      credit_network: @credit_network
-    }.to_json
+    hash.to_json
   end
 
   # returns all card information as single string
@@ -31,6 +28,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    card_s.from_json
   end
 
   # return a hash of the serialized credit card object
@@ -39,6 +37,8 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    { number: @number, expiration_date: @expiration_date,
+      owner: @owner, credit_network: @credit_network }
   end
 
   # return a cryptographically secure hash
@@ -46,5 +46,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    RbNaCl::Hash.sha256(to_json)
   end
 end
--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,17 +1,29 @@
 require 'rbnacl/libsodium'
+require 'base64'
 
+# Library for Symmetric Encrypting using Symmetric Cipher
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+    simple_key = Base64.decode64(key)
+    simplebox = RbNaCl::SimpleBox.from_secret_key(simple_key)
+    ciphertext = simplebox.encrypt(document)
+    Base64.encode64(ciphertext)
   end
 
-  def self.decrypt(encrypted_cc, key)
+  def self.decrypt(aes_crypt, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
+    simple_key = Base64.decode64(key)
+    ciphertext = Base64.decode64(aes_crypt)
+    simplebox = RbNaCl::SimpleBox.from_secret_key(simple_key)
+    simplebox.decrypt(ciphertext)
   end
 end
