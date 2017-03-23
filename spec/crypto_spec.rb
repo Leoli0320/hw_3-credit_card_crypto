de9661d5fcbe05cbd748bcaf501ae2f6654d4715
LeoLee <leo.li@iss.nthu.edu.tw>
2017-03-23 22:28:06 +0800

    FIX rubocop, COMPLETE spec & hash

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher'
+require_relative '../sk_cipher'
 require 'minitest/autorun'
-
 describe 'Test card info encryption' do
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
@@ -47,6 +47,22 @@ describe 'Test card info encryption' do
     it 'should decrypt text' do
       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
       dec = DoubleTranspositionCipher.decrypt(enc, @key)
+      dec.must_equal @cc.to_s
+    end
+  end
+
+  describe 'Using Modern Symmetric Cipher' do
+    it 'should encrypt card information' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc, key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc, key)
+      dec = ModernSymmetricCipher.decrypt(enc, key)
       dec.must_equal @cc.to_s
     end
   end
--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -3,35 +3,50 @@ require 'minitest/autorun'
 
 # Feel free to replace the contents of cards with data from your own yaml file
 card_details = [
-  { num: '4916603231464963', exp: 'Mar-30-2020', name: 'Soumya Ray', net: 'Visa' },
-  { num: '6011580789725897', exp: 'Sep-30-2020', name: 'Nick Danks', net: 'Visa' },
-  { num: '5423661657234057', exp: 'Feb-30-2020', name: 'Lee Chen', net: 'Mastercard' }
+  { num: '4916603231464963', exp: 'Mar-30-2020',
+    name: 'Soumya Ray', net: 'Visa' },
+  { num: '6011580789725897', exp: 'Sep-30-2020',
+    name: 'Nick Danks', net: 'Visa' },
+  { num: '5423661657234057', exp: 'Feb-30-2020',
+    name: 'Lee Chen', net: 'Mastercard' }
 ]
 
-cards = card_details.map { |c| CreditCard.new(c[:num], c[:exp], c[:name], c[:net]) }
+cards = card_details.map do |c|
+  CreditCard.new(c[:num], c[:exp], c[:name], c[:net])
+end
 
 describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
-    describe 'Check hashes are consistently produced' do
+    it 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      cards.map(&:hash).must_equal cards.map(&:hash)
     end
 
-    describe 'Check for unique hashes' do
+    it 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      hashed_cards = cards.map(&:hash)
+      hashed_cards.combination(2).any? { |a, b| a == b }.must_equal false
     end
   end
 
   describe 'Test cryptographic hashing' do
-    describe 'Check hashes are consistently produced' do
+    it 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      cards.map(&:hash_secure).must_equal cards.map(&:hash_secure)
     end
 
-    describe 'Check for unique hashes' do
+    it 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      secure_hashed_cards = cards.map(&:hash_secure)
+      secure_hashed_cards.combination(2).any? { |a, b| a == b }.must_equal false
     end
 
-    describe 'Check regular hash not same as cryptographic hash' do
+    it 'Check regular hash not same as cryptographic hash' do
       # TODO: Check that each card's hash is different from its hash_secure
+      hashed_cards = cards.map(&:hash)
+      secure_hashed_cards = cards.map(&:hash_secure)
+      identical = hashed_cards.zip(secure_hashed_cards).map { |x, y| x == y }
+      identical.each { |boolean| boolean.must_equal false }
     end
   end
 end
